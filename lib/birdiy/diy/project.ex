a83e20919e12e5d6bc95ccc0de5bd0f48868245b
Mike Chen <ccmikechen@gmail.com>
2019-04-11 20:54:56 +0800

    Add many fields in Project schema

--- lib/birdiy/diy/project.ex
+++ lib/birdiy/diy/project.ex
@@ -2,12 +2,17 @@ defmodule Birdiy.Diy.Project do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Birdiy.{Accounts, Diy}
+
   schema "projects" do
     field :introduction, :string
     field :name, :string
     field :tip, :string
-    belongs_to :author, Birdiy.Accounts.User
-    belongs_to :category, Birdiy.Diy.ProjectCategory
+    belongs_to :author, Accounts.User
+    belongs_to :category, Diy.ProjectCategory
+    has_many :materials, Diy.ProjectMaterials
+    has_many :file_resources, Diy.ProjectFileResource
+    has_many :methods, Diy.ProjectMethod
 
     timestamps()
   end
