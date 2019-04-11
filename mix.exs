80ed40274ef12daf812be60511d2a9fba4d98462
Mike Chen <ccmikechen@gmail.com>
2019-04-11 17:00:13 +0800

    Add absinthe dependencies

--- mix.exs
+++ mix.exs
@@ -40,7 +40,11 @@ defmodule Birdiy.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.4.16"},
+      {:absinthe_plug, "~> 1.4.6"},
+      {:absinthe_phoenix, "~> 1.4.3"},
+      {:absinthe_relay, "~> 1.4.6"},
     ]
   end
 
