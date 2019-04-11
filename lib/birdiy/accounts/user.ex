feba57f962e08713d38fff4bf79815511a6aa538
Mike Chen <ccmikechen@gmail.com>
2019-04-11 20:52:31 +0800

    Add many fields in User schema

--- lib/birdiy/accounts/user.ex
+++ lib/birdiy/accounts/user.ex
@@ -2,9 +2,40 @@ defmodule Birdiy.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Birdiy.{Accounts, Diy, Timeline}
+
   schema "users" do
     field :image, :string
     field :name, :string, size: 20
+    has_many :projects, Diy.Project, foreign_key: :author_id
+    has_many :posts, Timeline.Post, foreign_key: :author_id
+
+    many_to_many :following_users,
+                 Accounts.User,
+                 join_through: "user_followings",
+                 join_keys: [following_id: :id, followed_id: :id],
+                 on_replace: :delete
+
+    many_to_many :followed_users,
+                 Accounts.User,
+                 join_through: "user_followings",
+                 join_keys: [followed_id: :id, following_id: :id],
+                 on_replace: :delete
+
+    many_to_many :favorite_projects,
+                 Diy.Project,
+                 join_through: "user_favorite_projects",
+                 on_replace: :delete
+
+    many_to_many :liked_projects,
+                 Diy.Project,
+                 join_through: "user_liked_projects",
+                 on_replace: :delete
+
+    many_to_many :viewed_projects,
+                 Diy.Project,
+                 join_through: "user_viewed_projects",
+                 on_replace: :delete
 
     timestamps()
   end
