0392d378322ff7dab6d5c4440b098697d49f7142
Mike Chen <ccmikechen@gmail.com>
2019-05-11 20:29:32 +0800

    Handle prod/staging config with ansible

--- config/prod.exs
+++ config/prod.exs
@@ -8,19 +8,7 @@ config :birdiy, BirdiyWeb.Endpoint,
 config :logger, level: :info
 config :phoenix, :serve_endpoints, true
 
-config :ex_aws,
-  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
-  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
-  region: "sgp1",
-  s3: [
-    scheme: "https://",
-    host: "birdiy-dev-trail.sgp1.digitaloceanspaces.com",
-    region: "sgp1"
-  ]
-
-# Configures Guardian
-config :birdiy, Birdiy.Guardian,
-  issuer: "birdiy",
-  secret_key: "z46Dp+Dy5xTYrNqiNqIjwtNfPyqwKCWzfsn9Wt1wodvnlRsBzQmWdY/GUcucdJKn"
+config :birdiy, Birdiy.Repo,
+  ssl: true
 
 import_config "prod.secret.exs"
