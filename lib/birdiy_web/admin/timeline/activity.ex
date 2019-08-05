15cf64f424a59d33fc460788a77d41ba89679d62
Mike Chen <ccmikechen@gmail.com>
2019-08-05 19:13:52 +0800

    Add Activities in admin panel

--- lib/birdiy_web/admin/timeline/activity.ex
+++ lib/birdiy_web/admin/timeline/activity.ex
@@ -2,5 +2,11 @@ defmodule BirdiyWeb.Admin.Timeline.Activity do
   use ExAdmin.Register
 
   register_resource Birdiy.Timeline.Activity do
+    form activity do
+      inputs do
+        input(activity, :project_id)
+        input(activity, :post_id)
+      end
+    end
   end
 end
