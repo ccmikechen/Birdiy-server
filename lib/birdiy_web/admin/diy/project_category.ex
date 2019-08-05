9f31fdabf286a67db7cb6067a2c4f23dd493f4fa
Mike Chen <ccmikechen@gmail.com>
2019-08-05 16:45:23 +0800

    Add ProjectCategory in admin panel

--- lib/birdiy_web/admin/diy/project_category.ex
+++ lib/birdiy_web/admin/diy/project_category.ex
@@ -2,5 +2,20 @@ defmodule BirdiyWeb.Admin.Diy.ProjectCategory do
   use ExAdmin.Register
 
   register_resource Birdiy.Diy.ProjectCategory do
+    show category do
+      attributes_table do
+        row(:id)
+        row(:name)
+        row(:image)
+      end
+
+      panel "Topics" do
+        table_for category.topics do
+          column(:name, fn topic ->
+            ExAdmin.Utils.link_to(topic.name, "/admin/project_topics/#{topic.id}")
+          end)
+        end
+      end
+    end
   end
 end
