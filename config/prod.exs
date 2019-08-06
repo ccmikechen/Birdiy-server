1e3421312097e9ec4b96c471469777b4b31007cf
Mike Chen <ccmikechen@gmail.com>
2019-08-07 00:47:41 +0800

    Fix deployment issue that still using old assets

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,9 @@ use Mix.Config
 config :birdiy, BirdiyWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
-  code_reloader: false
+  code_reloader: false,
+  root: ".",
+  version: Mix.Project.config[:version]
 
 config :logger, level: :info
 config :phoenix, :serve_endpoints, true
